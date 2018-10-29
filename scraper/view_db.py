b09a55b81d14f4e30fb6a6fced41753e0c48d867
unknown <Ofer@Nahum-X1.ornim.medical>
2018-10-29 14:11:31 +0200

    Finale_1.0

---
+++ scraper/view_db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+with open('host_ip.txt', 'r') as ip_file:
+	HOST = ip_file.read()
+USER = 'root'
+PASSWD = '123'
+DATABASE = 'github_scrape'
+
+connection = mysql.connector.connect(host=HOST, user=USER, passwd=PASSWD, database=DATABASE) #connects to github_scrape database
+cursor = connection.cursor()
+
+cursor.execute('SELECT * FROM selenium')
+
+for row in cursor:
+	print(row)
